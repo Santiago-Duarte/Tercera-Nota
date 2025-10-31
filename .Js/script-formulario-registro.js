16673c9acefdc72c112d454400206b66aa5ee13c
Santiago Duarte <Santiago.duarteq@gmail.com>
2025-10-31 16:34:31 -0500

    Se renombran las carpetas

--- .Js/script-formulario-registro.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("registrationForm").addEventListener("submit", function(event) {
-    if (!this.checkValidity()) {
-        event.preventDefault();
-        alert("Por favor, completa el formulario correctamente.");
-    }
-});
